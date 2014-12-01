8ed72b2371db8e97c5ae5596646441e9cdaaa148
sowiho <soren@heidi-leisner.com>
2014-12-01 13:42:56 +0100

    gsooooooon

--- TestProject2/src/main/java/dk/groupie/AlleErFlyvende.java
+++ TestProject2/src/main/java/dk/groupie/AlleErFlyvende.java
@@ -1,9 +1,12 @@
 package dk.groupie;
 
+import com.google.gson.Gson;
+
 public class AlleErFlyvende {
 
   public static void main(String[] args) {
     System.out.println("Det virker!");
+      Gson prut = new Gson();
   }
 
 }
